bb7a3a1d01741e851e55c6bd8d8a14b5796802b9
Geraldopereirads <geraldo.pereirads@hotmail.com>
2023-05-26 14:09:59 -0300

    fix: reapir img

--- src/pages/HomePage/MainHomePage/styleMain.ts
+++ src/pages/HomePage/MainHomePage/styleMain.ts
@@ -45,13 +45,18 @@ export const StyleMainContainer = styled.main`
   img {
     width: 62%;
     padding-top: 40px;
+    margin: 0 auto;
   }
 
+  @media (max-width: 420px) {
+    img {
+      width: 80px;
+    }
+  }
   @media (min-width: 430px) {
     img {
       padding-top: 30px;
       width: 62%;
-      margin: 0 auto;
     }
 
     h1 {
@@ -137,20 +142,17 @@ export const StyleMainContainer = styled.main`
     }
   }
 
-
   @media (min-width: 980px) {
     img {
       width: 80%;
     }
   }
 
-
   @media (min-width: 980px) {
     img {
       width: 65%;
     }
   }
-
 
   @media (min-width: 1100px) {
     img {
@@ -173,9 +175,8 @@ export const StyleDivMainContainer = styled.div`
   @media (min-width: 700px) {
     padding-left: 30px;
   }
-  
+
   @media (min-width: 700px) {
     padding-left: 30px;
   }
-
 `;
